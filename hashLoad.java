81759b3300215e3afd3b3fc0c1576a83744f16db
Gerald Lim <s3560494@student.rmit.edu.au>
2018-05-19 21:32:51 +1000

    added variables, base structure of program

--- hashLoad.java
+++ hashLoad.java
@@ -3,8 +3,23 @@ import java.util.*;
 
 public class hashLoad
 {
+    // found this prime number from searching
+    private static int hashSize = 3145739;
+    private static int recordSize = 275;
+    static int pageSize;
+    static long start;
+
     public static void main(String[] args)
     {
+        RandomAccessFile inputFile = null;
+        RandomAccessFile outputFile = null;
+        int pageOffset = 0;
+        int recordOffset = 0;
+        int noRecords = pageSize / recordSize;
+        int remainingPages = pageSize % recordSize;
+        int currRec = 0;
+        byte[] emptyByte = new byte[200];
+
         if(args.length == 1)
         {
             String page = args[0];
@@ -19,5 +34,6 @@ public class hashLoad
         {
             System.out.println("Page size must be int");
         }
-    }
+
+        
 }
