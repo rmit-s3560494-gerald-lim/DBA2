3374a0d2b5f7b4a162f8bf3b2c0af8e0db423435
Gerald Lim <s3560494@student.rmit.edu.au>
2018-05-21 13:45:39 +1000

    Created base program taking in terminal arguments

--- hashQuery.java
+++ hashQuery.java
@@ -3,5 +3,22 @@ import java.util.*;
 
 public class hashQuery
 {
-    
+    private static int hashSize = 3145739;
+    private static int recordSize = 275;
+    static int pageSize;
+    static long start;
+    public static void main(String[] args)
+    {
+        if(args.length == 2)
+        {
+            String query = args[0];
+            String page = args[1];
+            try
+            {
+                pageSize = Integer.parseInt(page);
+            } catch (IndexOutOfBoundsException e) {
+                System.err.println("Page size must be int");
+            }
+        }
+    }
 }
