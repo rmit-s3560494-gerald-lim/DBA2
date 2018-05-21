4b90826dff0ffff85cdd6f4714f1cc084b4cf35a
Gerald Lim <s3560494@student.rmit.edu.au>
2018-05-21 14:07:31 +1000

    created vars for files and set to read

--- hashQuery.java
+++ hashQuery.java
@@ -9,6 +9,8 @@ public class hashQuery
     static long start;
     public static void main(String[] args)
     {
+        RandomAccessFile hashFile = null;
+        RandomAccessFile heapFile = null;
         if(args.length == 2)
         {
             String query = args[0];
@@ -17,8 +19,28 @@ public class hashQuery
             {
                 pageSize = Integer.parseInt(page);
             } catch (IndexOutOfBoundsException e) {
-                System.err.println("Page size must be int");
+                
+                e.printStackTrace();
             }
+        }
+        else
+        {
+            System.err.println("Page size must be int");
+        }
+
+        int pageCounter = 0;
+        int noRecords = pageSize / recordSize;
+        int currentRecord = 0;
+        byte[] emptyArray = new byte[200];
+        
+        byte[] bQuery = Arrays.copyOf(query.getBytes(), 200);
+        try
+        {
+            start = System.currentTimeMillis();
+            hashFile = new RandomAccessFile("hash." + pageSize + ".dat", "r");
+            heapFile = new RandomAccessFile("heap." + pageSize + ".dat", "r");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
